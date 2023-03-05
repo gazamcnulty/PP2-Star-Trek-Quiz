5b425d73273906811cba8744fc3d446be34b38d1
gazamcnulty <gazamcnulty@gmail.com>
2023-03-05 18:52:36 +0000

    used object loop to replace question

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,15 @@
 console.log("hello friend");
 
-let question1 = {
+let firstQuestion = {
     question: "What is the name of Data's cat?",
     answerX: 'Fluffy',
     answerCorrect: 'Spot',
     answerY: "Lieutenant Commander Whiskers",
     answerZ: 'William'
 };
-let quizC = question1.question;
-console.log(quizC);
 
-let question2 = {
+
+let secondQuestion = {
     question: "What is Picard's favourite beverage?",
     answerX: 'Black coffee',
     answerY: 'Hot chocolate',
@@ -18,13 +17,15 @@ let question2 = {
     answerZ: 'Espresso'
 };
 
-let question3 = {
+let thirdQuestion = {
     question: "What is the name of the Klingon sword?",
     answerCorrect: "Bat'leth",
     answerX: "Qaplah",
     answerY: "Lightsabre",
     answerZ: "Bird of prey"
 }
+
+let allQuestions = [firstQuestion, secondQuestion, thirdQuestion];
 
 let score = 0;
 
@@ -36,16 +37,15 @@ document.addEventListener("DOMContentLoaded", function() {
     welcomeA.innerHTML = `
     <img src="assets/images/enterprise-front.JPG" alt="a starship in space" id="image">
     <h3 id="welcome">Click the button to start the game!</h3>
-            <button type="submit" id="start" onclick="startGame();">Start Game</button>
+            <button type="submit" id="start" onclick="runGame();">Start Game</button>
             `;
     }
 )
 
 
-function startGame() {
+/*function startGame() {
     let y = document.body;
-    let welcomeA = document.getElementById('welcome-area');
-    welcomeA.remove();
+    
 
     let imageA = document.getElementById('quiz-image');
     imageA.innerHTML = `
@@ -54,7 +54,7 @@ function startGame() {
 
     let quizA = document.getElementById('quiz-area');
 
-    let quizB = `
+    /*let quizB = `
     <h3 id="question">What is the name of Data's cat?</h3>      
     <div id="quiz-answers">             
         <label for="answerA">Felix</label>             
@@ -72,8 +72,42 @@ function startGame() {
     `;
     quizA.innerHTML = quizB;
 
+    
 
 
+}
+*/
+
+
+
+function runGame() {
+    let welcomeA = document.getElementById('welcome-area');
+    welcomeA.remove();
+
+        let i = 0;
+        let qq = allQuestions[i].question;
+        let aa = allQuestions[i][1,2,3];
+        let quizD = document.createElement("h3");
+        let quizA = document.getElementById('quiz-area');
+        quizD.innerHTML = qq;
+        quizA.appendChild(quizD);
+
+        let answerList = document.createElement("div");
+        let answerText = `
+        <label for="answerA">${allQuestions[i].answerX}</label>             
+        <input type="radio" name="answer"  id="answerA>              
+        <label for="correctAnswer">${allQuestions[i].answerCorrect}</label>              
+        <input type="radio" name="answer"  id="correct-answer">              
+        <label for="answerC">${allQuestions[i].answerY}</label>              
+        <input type="radio" name="answer"  id="answerC">             
+        <label for="answerD">${allQuestions[i].answerZ}</label>             
+        <input type="radio" name="answer" id="answerD">
+        <div id="button-area">           
+            <button type="submit" id="submit" onclick="answerCheck();">Submit Answer</button>
+        </div>   
+        `;
+        answerList.innerHTML = answerText;
+        quizA.appendChild(answerList);
 }
 
 function answerCheck() {
